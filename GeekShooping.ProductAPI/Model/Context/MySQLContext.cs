3430bcfa9a5d0c7ea64166dad1dea1d092cfaf45
Carlos Wagner <carlos.sperling@granter.com.br>
2024-11-02 14:02:38 -0300

    - Iniciado API de Produto e projeto web.

---
+++ GeekShooping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+//add-migration AddProductDataTableOnDB
+//Update-Database -Migration AddProductDataTableOnDB
