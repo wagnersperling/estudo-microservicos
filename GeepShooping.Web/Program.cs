9223adbf366597641c820253fe454584ae50f596
Carlos Wagner <carlos.sperling@granter.com.br>
2024-11-07 12:14:50 -0300

    - Inserido service do Produto no projeto web.

--- GeepShooping.Web/Program.cs
+++ GeepShooping.Web/Program.cs
@@ -1,7 +1,12 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+        c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"]));
 
 var app = builder.Build();
 
